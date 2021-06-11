df20f2b5fdbcbcde14332bf98a9c9205f5214614
J Charles de Vera <jcharlesdevera@Js-MacBook-Pro.net>
2021-06-11 10:36:16 -0700

    updated more table

--- src/client/components/resultsComponents/tables.tsx
+++ src/client/components/resultsComponents/tables.tsx
@@ -38,7 +38,7 @@ const DenseTable: (props: { routeData: PulledDataFromTest; routeName?: string; s
         rpsArr.push(rps);
     });
 
-    if(!singleRoute){
+    if(singleRoute){
         // pushing function names of the first rps group at routenmae
         routeData[rpsArr[0]][routeName as string].middlewares.forEach((middlewareObj) => {
             resultArr.push({ fnName: middlewareObj.fnName, elapsedTimes: [] });
@@ -56,41 +56,75 @@ const DenseTable: (props: { routeData: PulledDataFromTest; routeName?: string; s
         for (const middlewareData of resultArr) {
             rows.push([middlewareData.fnName, ...middlewareData.elapsedTimes.map((e) => e.toString())]);
         }
-    }else {
-        const resultObj: {
-            [key: string]: {
-                receivedTimes: number[];
-                successfulCounts: number[];
-                errorCounts: number[];
-            };
-        } = {};
-        
-        Object.keys(routeData).forEach((rps) => {
-            rpsArr.push(rps);
-            // then for each key, pull the route as a label
-            //add to object as a property
-            //then grab recieved time and put in an array for value in onj
-            Object.keys(routeData[rps]).forEach((route) => {
-                if (!resultObj[route]) {
-                    resultObj[route] = {
-                        receivedTimes: [],
-                        successfulCounts: [],
-                        errorCounts: [],
-                    };
-                }
-                resultObj[route].receivedTimes.push(routeData[rps][route].receivedTime as number);
-                resultObj[route].errorCounts.push(routeData[rps][route].errorCount as number);
-                resultObj[route].successfulCounts.push(routeData[rps][route].successfulResCount as number)
-            })
-          });
-
-        rowsHeaders = ['', ...rpsArr];
-        for (const route in resultObj) {
-            for(const rowData in resultObj[route]){
-                rows.push(resultObj[route as string][rowData] as number[]);
-            }
-        }
     }
+    //     const resultObj: {
+    //         [key: string]: {
+    //             receivedTimes: number[];
+    //             successfulCounts: number[];
+    //             errorCounts: number[];
+    //         };
+    //     } = {};
+        
+    //     Object.keys(routeData).forEach((rps) => {
+    //         rpsArr.push(rps);
+    //         // then for each key, pull the route as a label
+    //         //add to object as a property
+    //         //then grab recieved time and put in an array for value in onj
+    //         Object.keys(routeData[rps]).forEach((route) => {
+    //             if (!resultObj[route]) {
+    //                 resultObj[route] = {
+    //                     receivedTimes: [],
+    //                     successfulCounts: [],
+    //                     errorCounts: [],
+    //                 };
+    //             }
+    //             resultObj[route].receivedTimes.push(routeData[rps][route].receivedTime as number);
+    //             resultObj[route].errorCounts.push(routeData[rps][route].errorCount as number);
+    //             resultObj[route].successfulCounts.push(routeData[rps][route].successfulResCount as number)
+    //         })
+    //       });
+
+    //     rowsHeaders = ['', ...rpsArr];
+    //     // for (const route in resultObj) {
+    //     //     for(const rowData in resultObj[route]){
+    //     //         rows.push([resultObj[route][rowData].map((e: number) => e.toString())]);
+    //     //     }
+    //     // }
+    //     return (
+    //         <TableContainer component={Paper}>
+    //             <Table className={classes.table} size="small" aria-label="a dense table">
+    //                 <TableHead>
+    //                     <TableRow></TableRow>
+    //                     <TableRow>
+    //                         {rowsHeaders.map((rps, i) => (
+    //                             <TableCell key={`rps-${i}`} align={i === 0 ? 'left' : 'right'}>
+    //                                 {i === 0 ? rps : `Interval at ${rps} RPS`}
+    //                             </TableCell>
+    //                         ))}
+    //                     </TableRow>
+    //                 </TableHead>
+    //                 <TableBody>
+    //                     {rows.map((row, i) => (
+    //                         <TableRow key={`table-${i}`}>
+    //                             {row.map((ele, j) => {
+    //                                 return j === 0 ? (
+    //                                     <TableCell key={`cell-${j}`} component="th" scope="row">
+    //                                         {ele}
+    //                                     </TableCell>
+    //                                 ) : (
+    //                                     <TableCell
+    //                                         align="right"
+    //                                         key={`cell-${j}`}
+    //                                     >{`${ele} ms`}</TableCell>
+    //                                 );
+    //                             })}
+    //                         </TableRow>
+    //                     ))}
+    //                 </TableBody>
+    //             </Table>
+    //         </TableContainer>
+    //     );
+    // }
     return (
         <TableContainer component={Paper}>
             <Table className={classes.table} size="small" aria-label="a dense table">
