3d9149d86ff436d48f1c5f142ebfb818307ef01a
Grigor Minasyan <grigorminasyan1998@gmail.com>
2021-06-24 21:31:48 -0700

    added supertest package, not yet finished with unit test

--- __tests__/allRPSfinished.ts
+++ __tests__/allRPSfinished.ts
@@ -1,14 +1,24 @@
+import express from 'express';
+
+import http, { createServer } from 'http';
+import { Server } from 'socket.io';
 import allRPSfinished from '../src/server/helpers/allRPSfinished';
 import AbortController from 'abort-controller';
-import http from 'http';
 import {
 	ioSocketCommands,
-	Jagtestercommands,
-	CollectedData,
+	// Jagtestercommands,
 	TimeArrRoutes,
 	TestConfigData,
 	GlobalVariables,
+	PulledDataFromTest,
 } from '../src/server/interfaces';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+const port = 15000;
+
+//------------------------
 
 const globalTestConfig: TestConfigData = {
 	rpsInterval: 100,
@@ -36,32 +46,53 @@ const timeArrRoutes: TimeArrRoutes = {
 		},
 	},
 };
+
+const pulledDataFromTest: PulledDataFromTest = {
+	'100': {
+		//used as route
+		'/': {
+			reqRoute: '/',
+			middlewares: [{ fnName: 'fnName', elapsedTime: 1 }],
+		},
+	},
+};
+
 let timoutCallCount = 0;
 const timeOutArray = [setTimeout(() => timoutCallCount++, 1000)];
 
-// const globalVariables: GlobalVariables = {
-// 	currentInterval: 100,
-// 	errorCount: 0,
-// 	successfulResCount: 100,
-// 	abortController: new AbortController(),
-// 	timeArrRoutes: timeArrRoutes,
-// 	timeOutArray: timeOutArray,
-// 	pulledDataFromTest: PulledDataFromTest,
-// 	isTestRunningInternal: boolean,
-// 	isTestRunningListener: (val: boolean) => {
-// 		io.emit(ioSocketCommands.testRunningStateChange, val);
-// 	},
-// 	isTestRunning: true,
-// 	agent: new http.Agent({ keepAlive: true }),
-// }
+const globalVariables: GlobalVariables = {
+	currentInterval: 100,
+	errorCount: 0,
+	successfulResCount: 100,
+	abortController: new AbortController(),
+	timeArrRoutes: timeArrRoutes,
+	timeOutArray: timeOutArray,
+	pulledDataFromTest: pulledDataFromTest,
+	isTestRunningInternal: true,
+	isTestRunningListener: (val: boolean) => {
+		io.emit(ioSocketCommands.testRunningStateChange, val);
+	},
+	isTestRunning: true,
+	agent: new http.Agent({ keepAlive: true }),
+};
+//---------------------
 
-const add = (a: number, b: number) => a + b;
+allRPSfinished(globalTestConfig, io, globalVariables);
 
-describe('test add function', () => {
-	it('should return 15 for add(10,5)', () => {
-		expect(add(10, 5)).toBe(15);
+httpServer.on('listening', function () {
+	// console.log(`Fake server running on http://localhost:${port}`);
+
+	const add = (a: number, b: number) => a + b;
+
+	describe('test add function', () => {
+		it('should return 15 for add(10,5)', () => {
+			expect(add(10, 5)).toBe(15);
+		});
+		it('should return 5 for add(2,3)', () => {
+			expect(add(2, 3)).toBe(5);
+		});
 	});
-	it('should return 5 for add(2,3)', () => {
-		expect(add(2, 3)).toBe(5);
-	});
+	httpServer.close();
 });
+
+httpServer.listen(port);
