5d6f828d7f3c252d4c6d05108642a3d0b6186e68
Grigor Minasyan <grigorminasyan1998@gmail.com>
2021-06-28 09:18:08 -0700

    merget grigors testing and jaosns testing

--- __tests-server__/mock-server.ts
+++ __tests-server__/mock-server.ts
@@ -7,16 +7,16 @@ const port = 5001;
 
 app.use(jagtester(app));
 
-app.get('/', 
-  // function fn1 (req, res, next) {console.log("fn1"); setTimeout(next, 200)},
-  // function fn2 (req, res, next) {console.log("fn2"); setTimeout(next, 400)},
-  // function fn3 (req, res, next) {console.log("fn3"); setTimeout(next, 600)},
-  (req, res) => {
-    res.sendStatus(200);
-});
-
+app.get(
+	'/',
+	// function fn1 (req, res, next) {console.log("fn1"); setTimeout(next, 200)},
+	// function fn2 (req, res, next) {console.log("fn2"); setTimeout(next, 400)},
+	// function fn3 (req, res, next) {console.log("fn3"); setTimeout(next, 600)},
+	(req, res) => {
+		res.sendStatus(200);
+	}
+);
 
 const mockServer = app.listen(port, () => console.log(`Running on on port ${port}`));
-
 
 export default mockServer;
