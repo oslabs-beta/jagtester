5a13976039f0009b969d8b6ce8a3c9792c779327
Grigor Minasyan <grigorminasyan1998@gmail.com>
2021-06-28 12:46:06 -0700

    merged singleRPSfinished test

--- __tests__/singleRPSfinished.ts
+++ __tests__/singleRPSfinished.ts
@@ -6,33 +6,43 @@ import {
 	io,
 	initializeVariables,
 	globalTestConfig,
-} from '../src/server/sampleTestVariables';
+} from '../__tests-server__/sampleTestVariables';
 
 jest.mock('node-fetch', () =>
 	jest.fn(() => {
 		return Promise.resolve({
-      json: () => Promise.resolve(globalVariables.pulledDataFromTest[globalVariables.currentInterval])
-    });
+			json: () =>
+				Promise.resolve(
+					globalVariables.pulledDataFromTest[globalVariables.currentInterval]
+				),
+		});
 	})
 );
 
-let originalPulledData
-describe('Testing singleRPSfinished functionality', () =>{
-  beforeEach(() =>{
-    initializeVariables();
-    originalPulledData = globalVariables.pulledDataFromTest;
-    globalVariables.pulledDataFromTest = {};
-    singleRPSfinished(globalTestConfig.rpsInterval, io, globalTestConfig, globalVariables)
-  });
-  it('should call emit singleRPS finished with rpsGroup', () => {
-    expect(io.emit).toBeCalledWith(ioSocketCommands.singleRPSfinished, globalTestConfig.rpsInterval)
-  });
-  it('should call fetch with correct header jagtester command and return correct response data', async () =>{
-    expect(fetch).toBeCalledWith(globalTestConfig.inputsData[0].targetURL, {
+let originalPulledData;
+describe('Testing singleRPSfinished functionality', () => {
+	beforeEach(() => {
+		initializeVariables();
+		originalPulledData = globalVariables.pulledDataFromTest;
+		globalVariables.pulledDataFromTest = {};
+		singleRPSfinished(globalTestConfig.rpsInterval, io, globalTestConfig, globalVariables);
+	});
+
+	it('should call emit singleRPS finished with rpsGroup', () => {
+		expect(io.emit).toBeCalledWith(
+			ioSocketCommands.singleRPSfinished,
+			globalTestConfig.rpsInterval
+		);
+	});
+
+	it('should call fetch with correct header jagtester command and return correct response data', async () => {
+		expect(fetch).toBeCalledWith(globalTestConfig.inputsData[0].targetURL, {
 			headers: {
 				jagtestercommand: Jagtestercommands.endTest.toString(),
-			}
-    }) 
-    expect(globalVariables.pulledDataFromTest[globalVariables.currentInterval]).toEqual(originalPulledData[globalVariables.currentInterval])
-  })
-})
+			},
+		});
+		expect(globalVariables.pulledDataFromTest[globalVariables.currentInterval]).toEqual(
+			originalPulledData[globalVariables.currentInterval]
+		);
+	});
+});
