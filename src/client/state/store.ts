e56855e56031c25311276fd065db9da3e094848d
Grigor Minasyan <grigorminasyan1998@gmail.com>
2021-06-28 17:06:01 -0700

    removed redux thunk

--- src/client/state/store.ts
+++ src/client/state/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import configReducer from './reducers/configReducer';
-import thunk from 'redux-thunk';
 
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
@@ -15,7 +14,6 @@ const persistedReducer = persistReducer(persistConfig, configReducer);
 const store = configureStore({
 	reducer: persistedReducer,
 	devTools: process.env.NODE_ENV !== 'production',
-	middleware: [thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
