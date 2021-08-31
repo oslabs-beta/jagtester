6b18bb13f6b0d851a83648b007993c70b1411e87
J Charles de Vera <jasoncdevera@icloud.com>
2021-08-31 15:14:27 -0700

    fixed typo

--- src/client/pages/testconfigpage.tsx
+++ src/client/pages/testconfigpage.tsx
@@ -12,33 +12,31 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const TestPage: () => JSX.Element = () => {
-	return (
-		<Row className="mx-4">
-			<Col className="col-12 col-xl-6">
-				<Card raised className="p-2">
-					<CardContent>
-						<TargetInputs />
-					</CardContent>
-				</Card>
-			</Col>
-			<Col className="col-12 col-xl-6">
-				<Card raised className="p-2">
-					<CardContent>
-						<RangeSliders />
-						<Buttons />
-						{process.env.JAG === 'demo' && (
-							<Typography paragraph variant="h4" align="center">
-								Warning! Currently running in demo mode and you cannot start any
-								tests. You can look around the app and check out our features. Take
-								a look at the results page and see what kinds of data we are
-								reporting. To use it, simpliy download from npm and use it locally.
-							</Typography>
-						)}
-					</CardContent>
-				</Card>
-			</Col>
-		</Row>
-	);
+  return (
+    <Row className="mx-4">
+      <Col className="col-12 col-xl-6">
+        <Card raised className="p-2">
+          <CardContent>
+            <TargetInputs />
+          </CardContent>
+        </Card>
+      </Col>
+      <Col className="col-12 col-xl-6">
+        <Card raised className="p-2">
+          <CardContent>
+            <RangeSliders />
+            <Buttons />
+            {process.env.JAG === 'demo' && (
+              <Typography paragraph variant="h5" align="center">
+                Warning! Currently running in demo mode and you cannot start any tests. You can look around the app and check out our features. Take a look at the results page and see what kinds of
+                data we are reporting. To use it, simply download from npm and use it locally.
+              </Typography>
+            )}
+          </CardContent>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default TestPage;
